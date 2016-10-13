5a97aea85b2249bb6880968895db9efdbec9b980
zwl <876143739@qq.com>
2016-10-13 15:11:40 +0800

    1.1.0

--- src/js/zcharts.js
+++ src/js/zcharts.js
@@ -1,12 +1,48 @@
 function zcharts(params) {
+
+    params.series.map(function (d,i) {
+
+    });
     var defaultOpts = {
-        
+        padding:{top:100,bottom:100,left:100,right:100},
+        container:{width:600,height:400},
+        color:['#95CCE6','#B6A2DE','#FED23D','#4087D7','#CBD87E'],
+        legend:[],
+        xAxis :
+        {
+            data : [],
+            name: "",
+            xAxisLable : {
+                rotate : 0,
+                margin : 0     //刻度标签与轴线之间的距离
+            }
+        },
+        yAxis : [
+            {
+                name: ""
+            }
+        ],
+        series: [
+            {
+                name:'融资数量',
+                type:'bar',
+                yAxisIndex:0,
+                data:[4,8,9]
+            },
+            {
+                name:'融资金额',
+                type:'bar',
+                yAxisIndex:1,
+                data:[18981,3310106,1199877]
+            }
+        ]
     };
+
     var zcharts = {};
 
     zcharts.drawSvg = function (id,option) {
         var opts = {};   //需要传递给画图区域的参数
-        
+
         var width = option.container.width,
             height = option.container.height,
             padding = option.padding;
@@ -20,13 +56,25 @@ function zcharts(params) {
             .attr("class","tooltip")
             .style("opacity",0.0);
 
+        /**
+         * 保存通用参数
+         * (1)画板及其长宽、边距
+         * (2)绘图数据
+         * (3)提示框tooltip
+         */
         opts.svg = svg;
-        opts.series = option.series;
-        opts.tooltip = tooltip;
         opts.width = width;
         opts.height = height;
         opts.padding = padding;
+        opts.series = option.series;
+        opts.legend = option.legend;
+        opts.tooltip = tooltip;
 
+        /**
+         * 判断绘图数据类型
+         * (1)饼图单独绘制（一条数据类型为饼图，其他数据类型必然为饼图）
+         * (2)柱状图、折线图可混合绘制
+         */
         if(option.series[0].type == 'pie'){
             option.series.map(function (d,i) {
                 d.data.map(function (v,j) {
@@ -37,23 +85,128 @@ function zcharts(params) {
             zcharts.drawPie(opts);
 
         }else{
-            option.series.map(function (d,i) {  //给每组数据插入颜色
-                d.color = option.color[i];
-            });
-
             var barData = [];
             var lineData = [];
-            var barLegend = [];   //绘制group bar时XScale定义域
+            var xAxisBarNum = [];   //绘制group bar时XScale定义域
 
-            //获取柱状图、折线图的独立数据
+            /**
+             * 处理柱状、折线的初始数据
+             * (1)获取柱状图、折线图的独立数据
+             * (2)获取x轴每个刻度的输入域——根据stack的值判断x轴每个刻度中柱状的个数
+             */
             option.series.map(function (d,i) {
+                d.color = option.color[i]; //给每组数据插入颜色
+
                 if(d.type == 'bar'){
                     barData.push(d);
-                    barLegend.push(d.name);
+
+                    /**
+                     * 获取xAxisBarNum的值
+                     * (1)当stack为空时，即没有设置值，则该柱状图单独绘制，name值插入xAxisBarNum
+                     * (2)当stack有值时，若xAxisBarNum中没有值与它相同，则插入stack的值，否则不插入。
+                     */
+                    if(!d.stack){
+                        xAxisBarNum.push(d.name);
+                    }else{
+                        if(!xAxisBarNum && !xAxisBarNum.length) {  /*xAxisBarNum为空时*/
+                            xAxisBarNum.push(d.stack);
+                        }else{
+                            var tag = 0;
+                            xAxisBarNum.map(function (v,j) {
+                                if(d.stack == v) {
+                                    tag = 1;
+                                }
+                            });
+                            if(tag == 0){
+                                xAxisBarNum.push(d.stack);
+                            }
+                        }
+                    }
+
                 }else if(d.type == 'line'){
                     lineData.push(d);
                 }
             });
+
+            /**堆叠布局函数*/
+            var stack = d3.layout.stack()   //构造一个新的默认的堆叠布局。
+                .values(function (v,j) { return v.data });
+
+            /**
+             * 堆叠布局
+             *          ——柱状图
+             * (1)处理barData，使其每一项的data的子项从数字变为对象.方便获取y0
+             * (2)给stack值为null的数据单独计算基线
+             * (3)单独获取stack值相同的需要堆叠的数据
+             * (4)给单独获取的堆叠数据计算基线——添加y0参数
+             */
+            var tempBar = {};  //将barData根据stack的值进行分组
+            barData.map(function (d,i) {
+                var objdata = [];
+                d.data.map(function (v,j) {
+                    var obj = {};
+                    obj.x = option.xAxis.data[j];
+                    obj.y = v;
+                    objdata.push(obj);
+                });
+                d.data = objdata;
+
+                /*将stack值相同的数据放在一个数组里*/
+                var stackName = d.stack || d.name;
+                tempBar[stackName] = tempBar[stackName] || [];
+                tempBar[stackName].push(d);
+            });
+
+            for(var i in tempBar){
+                var d = tempBar[i];
+                stack(d);
+                /*将获取到y0的数据塞回原数据barData*/
+                d.map(function (v,j) {
+                    barData.map(function (m,k) {
+                        if(m.name == v.name){
+                            m.data = v.data;
+                        }
+                    });
+                });
+            }
+
+            /**
+             * 堆叠布局
+             *          ——折线图
+             * (1)处理lineData，使其每一项的data的子项从数字变为对象.方便获取y0
+             * (2)给stack值为null的数据单独计算基线
+             * (3)单独获取stack值相同的需要堆叠的数据
+             * (4)给单独获取的堆叠数据计算基线——添加y0参数
+             */
+            var tempLine = {};  //将barData根据stack的值进行分组
+            lineData.map(function (d,i) {
+                var objdata = [];
+                d.data.map(function (v,j) {
+                    var obj = {};
+                    obj.x = option.xAxis.data[j];
+                    obj.y = v;
+                    objdata.push(obj);
+                });
+                d.data = objdata;
+
+                /*将stack值相同的数据放在一个数组里*/
+                var stackName = d.stack || d.name;
+                tempLine[stackName] = tempLine[stackName] || [];
+                tempLine[stackName].push(d);
+            });
+
+            for(var i in tempLine){
+                var d = tempLine[i];
+                stack(d);
+                /*将获取到y0的数据塞回原数据barData*/
+                d.map(function (v,j) {
+                    lineData.map(function (m,k) {
+                        if(m.name == v.name){
+                            m.data = v.data;
+                        }
+                    });
+                });
+            }
 
             /**
              * 绘制x轴比例尺
@@ -66,8 +219,8 @@ function zcharts(params) {
                 .domain(x.xLabel)
                 .rangeRoundBands([0,width-padding.left-padding.right],.1);
             x.xAxisScale = d3.scale.ordinal()  //柱状图x区间段比例尺
-                .domain(barLegend)    //legend为数据的名称
-                .rangeRoundBands([0,x.xScale.rangeBand()]);
+                .domain(xAxisBarNum)    //xAxisBarNum为数据的名称
+                .rangeRoundBands([0,x.xScale.rangeBand()],.1);
             x.xAxis = d3.svg.axis()
                 .scale(x.xScale)
                 .orient('bottom')
@@ -84,7 +237,7 @@ function zcharts(params) {
 
             option.series.map(function (d,i) {
                 var max = d3.max(d.data,function (v,j) {
-                    return v;
+                        return v.y + v.y0;
                 });
                 if(d.yAxisIndex == 0){
                     y0Max = y0Max > max ? y0Max : max;
@@ -127,6 +280,13 @@ function zcharts(params) {
                 .append('text')
                 .text(x.xName)
                 .attr('transform','translate(' + (width - padding.right - padding.left) + ',' + 25 + ')');
+
+
+            //旋转x轴刻度尺
+            d3.selectAll("g.x g.tick text")
+                .attr("x", -option.xAxis.xAxisLable.margin)
+                .attr("transform", "rotate(" + option.xAxis.xAxisLable.rotate + ")");
+
             //添加y轴
             option.yAxis.map(function (d,i) {
                 var yaxis;
@@ -146,6 +306,11 @@ function zcharts(params) {
                     .attr('transform','translate(' + -25 + ',' + -15 + ')');
             });
 
+            /**
+             * 保存柱状图、折线图的绘制参数
+             * (1)x、y轴数据-比例尺、标签...
+             * (2)绘图数据
+             */
             opts.x = x;
             opts.y = y;
             opts.barData = barData;
@@ -168,6 +333,7 @@ function zcharts(params) {
             height : null,
             padding : null,
             barData : null,
+            xAxisBarNum : null,
             x : null,
             y : null
         };
@@ -181,45 +347,48 @@ function zcharts(params) {
             y = option.y || defaultOpts.y;
 
         barData.map(function (d,i) {
+            var group = svg.append("g")  //用g表示一个数据组，每个数据组里是对应数据块
+                .attr("class","rectGroup")
+                .attr("fill",d.color)
+                .attr("transform","translate(" + padding.left + "," + padding.top + ")");
+
             //画柱状图
+            var rect = group.selectAll('.rect' + i )
+                .data(d.data)
+                .enter()
+                .append('rect')
+                .attr('width',x.xAxisScale.rangeBand()) //柱状宽度
+                .attr('fill',d.color);  //颜色
+
+            if(d.stack){
+                rect.attr('x',function (v,j) {  //柱状x轴偏移
+                    return x.xScale(v.x) + x.xAxisScale(d.stack);
+                });
+            }else{
+                rect.attr('x',function (v,j) {  //柱状x轴偏移
+                    return x.xScale(v.x) + x.xAxisScale(d.name);
+                });
+            }
             if(d.yAxisIndex == 0){
-                svg.selectAll('.rect' + i )
-                    .data(d.data)
-                    .enter()
-                    .append('rect')
-                    .attr('transform','translate(' + padding.left + ',' + padding.top + ')')
-                    .attr('x',function (v,j) {  //柱状x轴偏移
-                        return x.xScale(x.xLabel[j]) + x.xAxisScale(d.name);
+                rect.attr('y',function (v,j) {  //柱状y轴偏移
+                        return y.yScale0(v.y0)-(height-padding.top - padding.bottom - y.yScale0(v.y));
                     })
-                    .attr('y',function (v,j) {  //柱状y轴偏移
-                        return y.yScale0(v);
-                    })
-                    .attr('width',x.xAxisScale.rangeBand()) //柱状宽度
-                    .attr('height',function (d,i) {   //柱状高度
-                        return height-y.yScale0(d)-padding.top-padding.bottom;
-                    })
-                    .attr('fill',d.color);  //颜色
+                    .attr('height',function (v,j) {   //柱状高度
+                        return height-padding.top - padding.bottom - y.yScale0(v.y);
+                    });
+
             }else if(d.yAxisIndex == 1){
-                svg.selectAll('.rect' + i )
-                    .data(d.data)
-                    .enter()
-                    .append('rect')
-                    .attr('transform','translate(' + padding.left + ',' + padding.top + ')')
-                    .attr('x',function (v,j) {  //柱状x轴偏移
-                        return x.xScale(x.xLabel[j]) + x.xAxisScale(d.name);
+                rect.attr('y',function (v,j) {  //柱状y轴偏移
+                        return y.yScale1(v.y + v.y0);
                     })
-                    .attr('y',function (v,j) {  //柱状y轴偏移
-                        return y.yScale1(v);
-                    })
-                    .attr('width',x.xAxisScale.rangeBand()) //柱状宽度
-                    .attr('height',function (d,i) {   //柱状高度
-                        return height-y.yScale1(d)-padding.top-padding.bottom;
-                    })
-                    .attr('fill',d.color);  //颜色
+                    .attr('height',function (v,j) {   //柱状高度
+                        return height-y.yScale1(v.y + v.y0)-padding.top-padding.bottom;
+                    });
             }
         });
         svg.selectAll("rect")
             .on("mouseover",function(v,j){
+                d3.select(this).attr('opacity',0.8);
                 /**
                  *鼠标移入时，
                  *（1）通过 selection.html() 来更改提示框的文字
@@ -227,7 +396,7 @@ function zcharts(params) {
                  *（3）设定提示框的透明度为0.6
                  */
                 tooltip.html(barData[Math.floor(j/x.xLabel.length)].name + "<br />" +
-                        x.xLabel[j%x.xLabel.length] + ":" + v)
+                        v.x + ":" + v.y)
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY + 20) + "px")
                     .style("opacity",0.6);
@@ -238,6 +407,7 @@ function zcharts(params) {
                     .style("top", (d3.event.pageY + 20) + "px");
             })
             .on("mouseout",function(d){
+                d3.select(this).attr('opacity',1);
                 /* 鼠标移出时，将透明度设定为0.0（完全透明）*/
                 tooltip.style("opacity",0.0);
             });
@@ -264,19 +434,19 @@ function zcharts(params) {
             if(d.yAxisIndex == 0){
                 line = d3.svg.line()
                     .x(function(v,j) {
-                        return x.xScale(x.xLabel[j]);
+                        return x.xScale(v.x);
                     })
                     .y(function(v,j) {
-                        return y.yScale0(v);
+                        return y.yScale0(v.y + v.y0);
                     })
                     .interpolate('linear');
             }else if(d.yAxisIndex == 1){
                 line = d3.svg.line()
                     .x(function(v,j) {
-                        return x.xScale(x.xLabel[j]);
+                        return x.xScale(v.x);
                     })
                     .y(function(v,j) {
-                        return y.yScale1(v);
+                        return y.yScale1(v.y + v.y0);
                     })
                     .interpolate('linear');
             }
@@ -308,9 +478,9 @@ function zcharts(params) {
                  *（3）设定提示框的透明度为0.6
                  */
                 d3.select(this).style('fill',lineData[Math.floor(j/x.xLabel.length)].color)
-                            .attr('r',3.5);
+                    .attr('r',3.5);
                 tooltip.html(lineData[Math.floor(j/x.xLabel.length)].name + "<br />" +
-                        x.xLabel[j%x.xLabel.length] + ":" + v)
+                        v.x + ":" + v.y)
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY + 20) + "px")
                     .style("opacity",0.6);
@@ -326,8 +496,8 @@ function zcharts(params) {
                 /* 鼠标移出时，将透明度设定为0.0（完全透明）*/
                 tooltip.style("opacity",0.0);
             });
-
     };
+
     zcharts.drawPie = function (option) {
         var defaultOpts = {
             svg : null,
@@ -370,6 +540,7 @@ function zcharts(params) {
                 .attr("transform","translate(" + (width * d.center[0]) + "," + (height * d.center[1]) + ")");
 
             arcs.on("mouseover",function(v,j){
+                    d3.select(this).attr('opacity',0.6);
                     /**
                      *鼠标移入时，
                      *（1）通过 selection.html() 来更改提示框的文字
@@ -388,6 +559,7 @@ function zcharts(params) {
                         .style("top", (d3.event.pageY + 20) + "px");
                 })
                 .on("mouseout",function(d){
+                    d3.select(this).attr('opacity',1);
                     /* 鼠标移出时，将透明度设定为0.0（完全透明）*/
                     tooltip.style("opacity",0.0);
                 });
@@ -397,7 +569,6 @@ function zcharts(params) {
                     return d.data[j].color;
                 })
                 .attr("d",function(v){
-                    console.log(v);
                     return arc(v);
                 });
 
@@ -423,7 +594,27 @@ function zcharts(params) {
         });
 
     };
-    zcharts.drawLegend = function (options) {
+    zcharts.drawLegend = function (option) {
+        var defaultOpts = {
+            svg : null,
+            tooltip : null,
+            padding : null,
+            lineData : null
+        };
+        var svg = option.svg || defaultOpts.svg,
+            tooltip = option.tooltip || defaultOpts.tooltip,
+            padding = option.padding || defaultOpts.padding,
+            lineData = option.lineData || defaultOpts.lineData,
+            barData = option.lineData || defaultOpts.barData;
+
+        var legend = svg.append('g')
+            .attr('class','legend')
+            .attr('transform','translate(' + padding.left + ',' + (height - padding.bottom) + ')');
+        legend.selectAll('rect')
+            .data(barData)
+            .enter()
+            .append('rect')
+            .attr();
 
     };
 
